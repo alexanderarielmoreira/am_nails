bdf89b17dacef2833aa84f2a512dbf36942908e6
Ariel <alarmoreira@gmail.com>
2022-09-10 16:00:52 -0300

    Se agregan templates, apps y configs. varias

--- pwdaia/pwdaia/settings/base.py
+++ pwdaia/pwdaia/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.servicios', 
+    'apps.about', 
 ]
 
 MIDDLEWARE = [
--- pwdaia/pwdaia/urls.py
+++ pwdaia/pwdaia/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),  
     path('admin/', admin.site.urls),
     path('servicios/', include('apps.servicios.urls')), 
+    path('about/', include('apps.about.urls')), 
 ]
